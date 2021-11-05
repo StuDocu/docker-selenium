8bb89bbc7b8821305429be4dba585b3c131a50e5
Albert Carreras <alb.carreras@gmail.com>
2021-11-05 14:44:15 +0100

    Builds both architectures with one command

--- build.sh
+++ build.sh
@@ -3,14 +3,17 @@ TAG=$2
 
 set -e
 
-cd ./Base && docker buildx build --platform linux/arm64 -t $NAMESPACE/selenium-base:$TAG . --push
+cd ./Base && docker buildx build --platform linux/arm64,linux/amd64 -t $NAMESPACE/selenium-base:$TAG . --push
+
+cd ../Hub && sh generate.sh $TAG $NAMESPACE studocu \
+   && docker buildx build --platform linux/arm64,linux/amd64 -t $NAMESPACE/selenium-hub:$TAG . --push
 
 cd ../NodeBase && sh generate.sh $TAG $NAMESPACE studocu \
-   && docker buildx build --platform linux/arm64 -t $NAMESPACE/selenium-node-base:$TAG . --push
+   && docker buildx build --platform linux/arm64,linux/amd64 -t $NAMESPACE/selenium-node-base:$TAG . --push
 
 cd ../NodeChromium && sh generate.sh $TAG $NAMESPACE studocu \
-   && docker buildx build --platform linux/arm64 -t $NAMESPACE/selenium-node-chromium:$TAG . --push
+   && docker buildx build --platform linux/arm64,linux/amd64 -t $NAMESPACE/selenium-node-chromium:$TAG . --push
 
 cd ../Standalone && sh generate.sh StandaloneChromium selenium-node-chromium $TAG $NAMESPACE studocu \
    && cd ../StandaloneChromium \
-   && docker buildx build --platform linux/arm64 -t $NAMESPACE/selenium-chrome:$TAG . --push
+   && docker buildx build --platform linux/arm64,linux/amd64 -t $NAMESPACE/selenium-chrome:$TAG . --push
